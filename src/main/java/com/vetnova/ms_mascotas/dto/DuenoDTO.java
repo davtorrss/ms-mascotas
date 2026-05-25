381d7a8cb700dc814fef2500ba68ac39db76ca6e
david <dav.torrealba@duocuc.cl>
2026-05-24 23:01:21 -0400

    final 1.1

--- src/main/java/com/vetnova/ms_mascotas/dto/DuenoDTO.java
+++ src/main/java/com/vetnova/ms_mascotas/dto/DuenoDTO.java
@@ -4,9 +4,12 @@ public class DuenoDTO {
     private Long id;
     private String rut;
     private String nombre;
+    private String telefono;
+    private String direccion;
 
     public DuenoDTO() {}
 
+    // Getters y Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
@@ -15,4 +18,10 @@ public class DuenoDTO {
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
 }
